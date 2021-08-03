b53794b57256169a17ff7321742e130ab92497f1
go-tech01 <zoneofgod@gmail.com>
2021-08-03 11:26:02 +0900

    8/3 4ban class commit 1

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,5 +1,18 @@
+# from django.urls import path
+# from django.views.generic import TemplateView
+#
+# app_name = 'articleapp'
+#
+# urlpatterns = [
+#     path('list/', TemplateView.as_view(templatesname='article/list.html'), name='list')
+# ]
+
+from django.urls import path
+from django.views.generic import TemplateView
+
 app_name = 'articleapp'
 
 urlpatterns = [
-
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list')
 ]
+
