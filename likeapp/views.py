4e386b9c2a382bdb40e91ee9f7f3459dd7604e01
go-tech01 <zoneofgod@gmail.com>
2021-08-25 17:10:00 +0900

    8/25 4ban class commit 2

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -11,23 +12,32 @@ from django.views.generic import RedirectView
 from articleapp.models import Article
 from likeapp.models import LikeRecord
 
+def db_transaction(user, article):
+    like_record = LikeRecord.objects.filter(user=user, article=article)
+    if like_record.exists():
+        like_record.delete()
+        article.like -= 1
+        article.save()
+        raise ValidationError('like already exists')
+    else:
+        LikeRecord(user=user, article=article).save()
+    article.like += 1
+    article.save()
+
+
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
     def get(self, request, *args, **kwargs):
         user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
-        like_record = LikeRecord.objects.filter(user=user, article=article)
-        if like_record.exists():
+
+        try:
+            db_transaction(user, article)
+            messages.add_message(request, messages.SUCCESS, "좋아요 했따링")
+        except:
             messages.add_message(request, messages.ERROR, "좋아요 취소...")
-            like_record.delete()
-            article.like -= 1
-            article.save()
-            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
-        article.like += 1
-        article.save()
-        messages.add_message(request, messages.SUCCESS, "좋아요 했따링")
+            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']}))
+
         return super().get(request, *args, **kwargs)
     def get_redirect_url(self, *args, **kwargs):
         return reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']})
